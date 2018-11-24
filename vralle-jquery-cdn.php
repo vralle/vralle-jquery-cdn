4a96f48c24abd981bb874da71b0a572c8c677eab
vralle <email4vit@gmail.com>
2018-11-24 21:22:04 +0300

    Version up

--- vralle-jquery-cdn.php
+++ vralle-jquery-cdn.php
@@ -5,7 +5,7 @@ namespace Vralle\Jquery\Cdn;
  * Plugin Name:         vralle.jQuery-CDN
  * Plugin URI:          https://github.com/vralle/vralle-jquery-cdn
  * Description:         A modern way to load jQuery from CDN with a local fallback
- * Version:             2018-10-28
+ * Version:             2018-11-24
  * Author:              V.Ralle
  * Author URI:          https://github.com/vralle/
  * License:             MIT
