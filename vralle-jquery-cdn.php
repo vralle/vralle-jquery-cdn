6a6cd6a9f9e2ddfdb4d324f19b6bf70b080e73f0
vralle <email4vit@gmail.com>
2019-05-17 20:28:22 +0300

    Add preload link

--- vralle-jquery-cdn.php
+++ vralle-jquery-cdn.php
@@ -41,6 +41,18 @@ function set_resource_hints($urls, $relation_type)
     return $urls;
 }
 
+\add_action('wp_head', __NAMESPACE__ . '\\add_preload_script', 1);
+function add_preload_script()
+{
+    if (is_admin() || is_customize_preview() || is_amp()) {
+        return;
+    }
+    $jquery_src = \wp_scripts()->registered['jquery']->src;
+    ?>
+<link href="<?php echo $jquery_src; ?>" rel="preload" as="script" crossorigin="anonymous">
+    <?php
+}
+
 \add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts', 100);
 function enqueue_scripts()
 {
