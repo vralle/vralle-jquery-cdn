390388146ac9b060c635726a0c1ff24edc289607
vralle <email4vit@gmail.com>
2019-12-16 22:54:50 +0300

    Plugin refactoring.

--- vralle-jquery-cdn.php
+++ vralle-jquery-cdn.php
@@ -1,103 +1,132 @@
 <?php
-namespace Vralle\Jquery\Cdn;
-
 /**
  * Plugin Name:         vralle.jQuery-CDN
  * Plugin URI:          https://github.com/vralle/vralle-jquery-cdn
  * Description:         A modern way to load jQuery from CDN with a local fallback
- * Version:             2019-05-23
+ * Version:             2019-12-17
  * Author:              V.Ralle
  * Author URI:          https://github.com/vralle/
  * License:             MIT
- * GitHub Plugin URI:   https://github.com/vralle/vralle-jquery-cdn.git
+ * GitHub Plugin URI:   https://github.com/vralle/vralle-jquery-cdn
  * Requires WP:         4.6
  * Requires PHP:        5.6
+ *
+ * @package vralle-jqury-cdn
  */
 
+namespace VralleJqueryCdn;
+
+use function add_action;
+use function add_filter;
+use function apply_filters;
+use function function_exists;
+use function esc_url;
+use function includes_url;
+use function is_admin;
+use function is_amp_endpoint;
+use function is_customize_preview;
+use function sprintf;
+use function version_compare;
+use function wp_add_inline_script;
+use function wp_deregister_script;
+use function wp_register_script;
+use function wp_script_is;
+use function wp_scripts;
+
 // If this file is called directly, abort.
-if (!defined('WPINC')) {
+if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-function is_amp()
-{
-    return function_exists('is_amp_endpoint') && is_amp_endpoint();
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\init' );
+
+/**
+ * Register all of the needed hooks and actions.
+ */
+function init() {
+    add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts', 11 );
+    add_filter( 'script_loader_tag', __NAMESPACE__ . '\\make_tag', 10, 3 );
+    add_action( 'wp_head', __NAMESPACE__ . '\\add_preload_link', 1 );
 }
 
-\add_filter('wp_resource_hints', __NAMESPACE__  . '\\set_resource_hints', 10, 2);
-function set_resource_hints($urls, $relation_type)
-{
-    if (is_admin() || is_amp()) {
-        return $urls;
+/**
+ * Register new src and ad fallback
+ */
+function enqueue_scripts() {
+    $jquery_script = wp_scripts()->registered['jquery-core'];
+    $jquery_src    = $jquery_script->src;
+    $jquery_ver    = $jquery_script->ver;
+
+    wp_deregister_script( 'jquery-core' );
+    wp_deregister_script( 'jquery' );
+
+    wp_register_script(
+        'jquery-core',
+        'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',
+        array(),
+        apply_filters( 'vralle_jquery_cdn_ver', '3.4.1', $jquery_ver ),
+        true
+    );
+
+    wp_register_script( 'jquery', false, array( 'jquery-core' ), $jquery_ver, true );
+
+    $site_url = site_url();
+
+    if ( ! $site_url ) {
+        $site_url = wp_guess_url();
     }
 
-    if (wp_script_is('jquery', 'queue')) {
-        if ($relation_type === 'preconnect') {
-            $urls[] = array(
-                'href' => 'https://ajax.googleapis.com',
-                'crossorigin' => 'anonymous',
-            );
-        }
-    }
-
-    return $urls;
+    $fallback = sprintf(
+        '(window.jQuery && jQuery.noConflict()) || document.write(\'<script src=\"%s\"><\/script>\')',
+        esc_url( $site_url . $jquery_src )
+    );
+    wp_add_inline_script( 'jquery', $fallback );
 }
 
-\add_action('wp_head', __NAMESPACE__ . '\\add_preload_script', 1);
-function add_preload_script()
-{
-    if (is_admin() || is_customize_preview() || is_amp()) {
+
+/**
+ * Make HTML tag
+ *
+ * @param string $tag    The <script> tag for the enqueued script.
+ * @param string $handle The script's registered handle.
+ * @param string $src    The script's source URL.
+ *
+ * @return string The script tag
+ */
+function make_tag( $tag, $handle, $src ) {
+    if ( is_admin() ) {
+        return $tag;
+    }
+    if ( 'jquery-core' === $handle ) {
+        $tag = sprintf(
+            '<script src="%s" crossorigin="anonymous"></script>',
+            esc_url( $src )
+        );
+    }
+
+    return $tag;
+}
+
+/**
+ * Add 'preload' meta link
+ */
+function add_preload_link() {
+    if ( is_admin() ) {
         return;
     }
-    if (wp_script_is('jquery', 'queue')) {
-        $jquery_src = \wp_scripts()->registered['jquery']->src;
+    if ( wp_script_is( 'jquery' ) ) {
+        $jquery_src = wp_scripts()->registered['jquery-core']->src;
         ?>
-<link href="<?php echo esc_url($jquery_src); ?>" rel="preload" as="script" crossorigin="anonymous">
+<link href="<?php echo esc_url( $jquery_src ); ?>" rel="preload" as="script" crossorigin="anonymous">
         <?php
     }
 }
 
-\add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts', 100);
-function enqueue_scripts()
-{
-    global $wp_version;
-    if (version_compare($wp_version, '5.2.1', '>=' )) {
-        $jquery_version = '3.4.1';
-    } else {
-        $jquery_version = \wp_scripts()->registered['jquery']->ver;
-    }
-    \wp_deregister_script('jquery');
-    \wp_register_script(
-        'jquery',
-        'https://ajax.googleapis.com/ajax/libs/jquery/' . $jquery_version . '/jquery.min.js',
-        [],
-        $jquery_version,
-        true
-    );
-
-    $jquery_fallback = \sprintf(
-        '(window.jQuery && jQuery.noConflict()) || document.write(\'<script src=\"%s\"><\/script>\')',
-        \esc_url(\includes_url('/js/jquery/jquery.js'))
-    );
-    \wp_add_inline_script('jquery', $jquery_fallback);
+/**
+ * Check is AMP endpoint
+ *
+ * @return boolean
+ */
+function is_amp() {
+    return function_exists( 'is_amp_endpoint' ) && is_amp_endpoint();
 }
-
-\add_filter(
-    'script_loader_tag',
-    function ($tag, $handle, $src) {
-        if (is_admin()) {
-            return $tag;
-        }
-        if ('jquery' === $handle) {
-            $tag = \str_replace(
-                '></script>',
-                ' crossorigin="anonymous"></script>',
-                $tag
-            );
-        }
-
-        return $tag;
-    },
-    10,
-    3
-);
